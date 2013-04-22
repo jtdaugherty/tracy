e2584406132b7eb0a3dc28cc6ee650fbf997cdb5
Jonathan Daugherty <cygnus@foobox.com>
2013-04-22 16:52:26 -0700

    Scenes cleanup

--- src/Tracy/Scenes.hs
+++ src/Tracy/Scenes.hs
@@ -33,37 +33,28 @@ vpd d cam = cam & (cameraData.pinholeVPDistance) .~ d
 eye :: V3 Double -> Camera Pinhole -> Camera Pinhole
 eye e cam = cam & (cameraData.pinholeEyePoint) .~ e
 
+world :: [Object] -> World
+world os = World { _viewPlane = defaultVp
+                 , _objects = os
+                 , _bgColor = cBlack
+                 }
+
 world1 :: (Camera Pinhole, World)
 world1 =
     let s = sphere (V3 0 0 0) 85.0 cRed
-    in ( camera
-       , World { _viewPlane = defaultVp
-               , _objects = [s]
-               , _bgColor = cBlack
-               }
-       )
+    in ( camera, world [s] )
 
 world2 :: (Camera Pinhole, World)
 world2 =
     let s = sphere (V3 0 0 11) 30.0 cBlue
         s2 = sphere (V3 0 0 0) 40.0 cGreen
-    in ( camera
-       , World { _viewPlane = defaultVp
-               , _objects = [s, s2]
-               , _bgColor = cBlack
-               }
-       )
+    in ( camera, world [s, s2] )
 
 world3 :: (Camera Pinhole, World)
 world3 =
     let s = sphere (V3 0 0 11) 30.0 cBlue
         p = plane (V3 0 0 0) (V3 0 1 0.1) cGreen
-    in ( camera
-       , World { _viewPlane = defaultVp
-               , _objects = [s, p]
-               , _bgColor = cBlack
-               }
-       )
+    in ( camera, world [s, p] )
 
 world4 :: (Camera Pinhole, World)
 world4 =
@@ -71,12 +62,7 @@ world4 =
         p = plane (V3 0 0 0) (V3 0 1 0.1) cGreen
         s2 = sphere (V3 50 5 0) 10.0 cMagenta
         s3 = sphere (V3 (-50) 10 0) 15.0 cYellow
-    in ( camera
-       , World { _viewPlane = defaultVp
-               , _objects = [s, p, s2, s3]
-               , _bgColor = cBlack
-               }
-       )
+    in ( camera, world [s, p, s2, s3] )
 
 scenes :: [(FilePath, (Camera Pinhole, World))]
 scenes =
