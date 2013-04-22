1bd2eeb5e44df8f2af88075c111aa0db2c8ea5fc
Jonathan Daugherty <cygnus@foobox.com>
2013-04-22 15:13:46 -0700

    Make AA sampling configurable

--- src/Main.hs
+++ src/Main.hs
@@ -12,18 +12,21 @@ import Tracy.Types
 data Arg = ShowLog
          | BeSilent
          | Help
+         | SampleRoot String
            deriving (Eq, Show)
 
 opts :: [OptDescr Arg]
 opts = [ Option "h" ["help"] (NoArg Help) "This help output"
        , Option "l" ["log"] (NoArg ShowLog) "Show log messages"
        , Option "q" ["quiet"] (NoArg BeSilent) "Operate silently (no output)"
+       , Option "a" ["aa-sample-root"] (ReqArg SampleRoot "ROOT") "AA sample root"
        ]
 
 updateConfig :: Config -> Arg -> Config
 updateConfig c Help = c
 updateConfig c ShowLog = c { showLog = True }
 updateConfig c BeSilent = c { showLog = False, silent = True }
+updateConfig c (SampleRoot s) = c { sampleRoot = read s }
 
 usage :: IO ()
 usage = do
