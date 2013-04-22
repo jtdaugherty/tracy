626b4c7043e086c1e692ed0b74676bbe241d7836
Jonathan Daugherty <cygnus@foobox.com>
2013-04-22 15:13:52 -0700

    Add world 4

--- src/Tracy/Scenes.hs
+++ src/Tracy/Scenes.hs
@@ -48,9 +48,23 @@ world3 =
              , _eyePoint = V3 0 0 100
              }
 
+world4 :: World
+world4 =
+    let s = sphere (V3 0 0 11) 30.0 cBlue
+        p = plane (V3 0 0 0) (V3 0 1 0.1) cGreen
+        s2 = sphere (V3 50 5 0) 10.0 cMagenta
+        s3 = sphere (V3 (-50) 10 0) 15.0 cYellow
+    in World { _viewPlane = defaultVp
+             , _objects = [s, p, s2, s3]
+             , _bgColor = cBlack
+             , _viewPlaneDistance = 200
+             , _eyePoint = V3 0 0 100
+             }
+
 scenes :: [(FilePath, World)]
 scenes =
     [ ("world1.bmp", world1)
     , ("world2.bmp", world2)
     , ("world3.bmp", world3)
+    , ("world4.bmp", world4)
     ]
