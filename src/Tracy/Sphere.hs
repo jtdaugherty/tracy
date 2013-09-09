1948aebd801662162d803af8ef3e1a9bb8318bad
Jonathan Daugherty <jtd@galois.com>
2013-09-08 21:51:35 -0700

    Factor out common sphere hit testing code

--- src/Tracy/Sphere.hs
+++ src/Tracy/Sphere.hs
@@ -1,5 +1,6 @@
 module Tracy.Sphere where
 
+import Control.Applicative
 import Control.Lens
 import Linear
 
@@ -15,7 +16,10 @@ sphere p rad m =
            }
 
 shadowHitSphere :: V3 Float -> Float -> Ray -> Maybe Float
-shadowHitSphere p rad ray =
+shadowHitSphere = _hitSphere
+
+_hitSphere :: V3 Float -> Float -> Ray -> Maybe Float
+_hitSphere p rad ray =
     let temp = ray^.origin - p
         a = (ray^.direction) `dot` (ray^.direction)
         b = (2 * temp) `dot` (ray^.direction)
@@ -36,25 +40,10 @@ hitSphere :: V3 Float -> Float -> Material -> Ray
           -> Maybe (Shade, Float)
 hitSphere p rad mat ray =
     let temp = ray^.origin - p
-        a = (ray^.direction) `dot` (ray^.direction)
-        b = (2 * temp) `dot` (ray^.direction)
-        c = (temp `dot` temp) - rad * rad
-        disc = b * b - 4 * a * c
-        e = sqrt disc
-        denom = 2 * a
-
-        t1 = (-b - e) / denom
-        t2 = (-b + e) / denom
-
         makeShade tval = (sh, tval)
             where
               sh = defaultShade { _localHitPoint = ray^.origin + (tval *^ ray^.direction)
                                 , _material = mat
                                 , _normal = (temp + (tval *^ ray^.direction)) ^/ rad
                                 }
-
-    in if t1 > epsilon
-       then Just $ makeShade t1
-       else if t2 > epsilon
-            then Just $ makeShade t2
-            else Nothing
+    in makeShade <$> _hitSphere p rad ray
