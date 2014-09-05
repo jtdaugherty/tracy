450ade40fbe26b2e97d78e54bc542671eff9fe51
Jonathan Daugherty <cygnus@foobox.com>
2014-09-04 20:39:15 -0700

    Main: factor out SetScene handling from localRenderThread

--- src/Tracy/Main.hs
+++ src/Tracy/Main.hs
@@ -121,41 +121,45 @@ render sceneName requestedChunks renderCfg s renderManager iChan dChan = do
   writeChan dChan DShutdown
   writeChan iChan IShutdown
 
+localSetSceneAndRender :: Chan JobRequest -> Chan JobResponse -> RenderConfig -> SceneDesc -> IO ()
+localSetSceneAndRender jobReq jobResp cfg sDesc = do
+    let Right builtScene = sceneFromDesc sDesc
+        squareSampler = regular
+        diskSampler = builtScene^.sceneCamera.cameraData.lensSampler
+        numSets = fromEnum $ sDesc^.sceneDescWorld^.wdViewPlane.hres
+        aScheme = builtScene^.sceneAccelScheme
+        worldAccel = (aScheme^.schemeApply) (builtScene^.sceneWorld)
+        worldAccelShadows = case cfg^.forceShadows of
+                              Nothing -> worldAccel
+                              Just v -> worldAccel & worldShadows .~ v
+        scene = builtScene & sceneWorld .~ worldAccelShadows
+
+    -- Generate sample data for square and disk samplers
+    sSamples <- replicateM numSets $ squareSampler (cfg^.sampleRoot)
+    dSamples <- replicateM numSets $ diskSampler (cfg^.sampleRoot)
+
+    let processRequests = do
+          ev <- readChan jobReq
+          case ev of
+              RenderRequest chunkId (start, stop) -> do
+                  ch <- renderChunk cfg scene (start, stop) sSamples dSamples
+                  let converted = (cdemote <$>) <$> ch
+                  writeChan jobResp $ ChunkFinished chunkId (start, stop) converted
+                  processRequests
+              RenderFinished -> do
+                  writeChan jobResp JobAck
+              _ -> writeChan jobResp $ JobError "Expected RenderRequest or RenderFinished, got unexpected event"
+
+    writeChan jobResp JobAck
+    processRequests
+
 localRenderThread :: Chan JobRequest -> Chan JobResponse -> IO ()
 localRenderThread jobReq jobResp = do
     let waitForJob = do
           reqEv <- readChan jobReq
           case reqEv of
               SetScene cfg sDesc -> do
-                  let Right builtScene = sceneFromDesc sDesc
-                      squareSampler = regular
-                      diskSampler = builtScene^.sceneCamera.cameraData.lensSampler
-                      numSets = fromEnum $ sDesc^.sceneDescWorld^.wdViewPlane.hres
-                      aScheme = builtScene^.sceneAccelScheme
-                      worldAccel = (aScheme^.schemeApply) (builtScene^.sceneWorld)
-                      worldAccelShadows = case cfg^.forceShadows of
-                                            Nothing -> worldAccel
-                                            Just v -> worldAccel & worldShadows .~ v
-                      scene = builtScene & sceneWorld .~ worldAccelShadows
-
-                  -- Generate sample data for square and disk samplers
-                  sSamples <- replicateM numSets $ squareSampler (cfg^.sampleRoot)
-                  dSamples <- replicateM numSets $ diskSampler (cfg^.sampleRoot)
-
-                  let processRequests = do
-                        ev <- readChan jobReq
-                        case ev of
-                            RenderRequest chunkId (start, stop) -> do
-                                ch <- renderChunk cfg scene (start, stop) sSamples dSamples
-                                let converted = (cdemote <$>) <$> ch
-                                writeChan jobResp $ ChunkFinished chunkId (start, stop) converted
-                                processRequests
-                            RenderFinished -> do
-                                writeChan jobResp JobAck
-                            _ -> writeChan jobResp $ JobError "Expected RenderRequest or RenderFinished, got unexpected event"
-
-                  writeChan jobResp JobAck
-                  processRequests
+                  localSetSceneAndRender jobReq jobResp cfg sDesc
                   waitForJob
               Shutdown -> do
                   writeChan jobResp JobAck
