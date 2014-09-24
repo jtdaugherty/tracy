5a2613a41915e16ec8c58cebf88634b0b38ae5ea
Jonathan Daugherty <cygnus@foobox.com>
2014-09-23 20:43:41 -0700

    Add scale, scaleUni transformations

--- src/Tracy/Transformations.hs
+++ src/Tracy/Transformations.hs
@@ -1,5 +1,7 @@
 module Tracy.Transformations
   ( translate
+  , scale
+  , scaleUni
   ) where
 
 import Linear
@@ -19,3 +21,20 @@ translate x y z =
       r1' = V4 1 0 0 (-x)
       r2' = V4 0 1 0 (-y)
       r3' = V4 0 0 1 (-z)
+
+scaleUni :: Float -> Transformation
+scaleUni f = scale f f f
+
+scale :: Float -> Float -> Float -> Transformation
+scale x y z =
+    Trans ( V4 r1 r2 r3 r4
+          , V4 r1' r2' r3' r4
+          )
+    where
+      r1 = V4 x 0 0 0
+      r2 = V4 0 y 0 0
+      r3 = V4 0 0 z 0
+      r4 = V4 0 0 0 1
+      r1' = V4 (1/x) 0 0 0
+      r2' = V4 0 (1/y) 0 0
+      r3' = V4 0 0 (1/z) 0
