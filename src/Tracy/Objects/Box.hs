443be5906369dd275cc8d2e19b74bb0fd8c31d30
Jonathan Daugherty <cygnus@foobox.com>
2014-10-09 19:14:18 -0700

    Box: implement bounding box to support putting Boxes in Grids

--- src/Tracy/Objects/Box.hs
+++ src/Tracy/Objects/Box.hs
@@ -11,23 +11,23 @@ import Tracy.Util
 
 box :: V3 Float -> V3 Float -> Material -> Object
 box p0 p1 m =
-    Object { _objectMaterial = m
-           , _hit = hitBox p0 p1 m
-           , _shadow_hit = shadowHitBox p0 p1
-           , _bounding_box = Nothing
-           , _areaLightImpl = Nothing
-           }
+    let bbox = boundingBox p0 p1
+    in Object { _objectMaterial = m
+              , _hit = hitBox bbox m
+              , _shadow_hit = shadowHitBox bbox
+              , _bounding_box = Just bbox
+              , _areaLightImpl = Nothing
+              }
 
-shadowHitBox :: V3 Float -> V3 Float -> Ray -> Maybe Float
-shadowHitBox p0 p1 r =
-   case boundingBoxHit (boundingBox p0 p1) r of
+shadowHitBox :: BBox -> Ray -> Maybe Float
+shadowHitBox bb r =
+   case boundingBoxHit bb r of
        Nothing -> Nothing
        Just (_, _, t, _) -> Just t
 
-hitBox :: V3 Float -> V3 Float -> Material -> Ray -> Maybe (Shade, Float)
-hitBox p0 p1 m r =
-    let bbox = boundingBox p0 p1
-    in case boundingBoxHit bbox r of
+hitBox :: BBox -> Material -> Ray -> Maybe (Shade, Float)
+hitBox bbox m r =
+    case boundingBoxHit bbox r of
         Nothing -> Nothing
         Just (_, faceNorm, t, localHP) ->
             let sh = defaultShade { _localHitPoint = localHP
