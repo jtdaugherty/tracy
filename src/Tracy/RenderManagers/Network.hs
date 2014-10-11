82c83bf3dfba4dc0dc9018966aa301dc716686e7
Jonathan Daugherty <jtd@galois.com>
2014-10-10 17:31:27 -0700

    Network: critical fix to use ZMQ withSocket

--- src/Tracy/RenderManagers/Network.hs
+++ src/Tracy/RenderManagers/Network.hs
@@ -13,36 +13,36 @@ import Tracy.Types
 networkNodeThread :: String -> Chan InfoEvent -> Chan JobRequest -> Chan JobResponse -> IO () -> IO ()
 networkNodeThread connStr iChan jobReq jobResp readyNotify = withContext $ \ctx -> do
     writeChan iChan $ IConnecting connStr
-    sock <- socket ctx Req
-    connect sock connStr
-    writeChan iChan $ IConnected connStr
+    withSocket ctx Req $ \sock -> do
+      connect sock connStr
+      writeChan iChan $ IConnected connStr
 
-    let worker = do
-          ev <- readChan jobReq
-          case ev of
-              SetScene cfg s -> do
-                  send sock [] $ encode $ SetScene cfg s
-                  _ <- receive sock
-                  worker
-              RenderRequest -> do
-                  _ <- send sock [] $ encode RenderRequest
-                  reply <- receive sock
-                  case decode reply of
-                      Left e -> writeChan jobResp $ JobError e
-                      Right r -> writeChan jobResp r
-                  readyNotify
-                  worker
-              RenderFinished -> do
-                  send sock [] $ encode RenderFinished
-                  _ <- receive sock
-                  readyNotify
-                  worker
-              Shutdown -> do
-                  send sock [] $ encode Shutdown
-                  _ <- receive sock
-                  return ()
+      let worker = do
+            ev <- readChan jobReq
+            case ev of
+                SetScene cfg s -> do
+                    send sock [] $ encode $ SetScene cfg s
+                    _ <- receive sock
+                    worker
+                RenderRequest -> do
+                    _ <- send sock [] $ encode RenderRequest
+                    reply <- receive sock
+                    case decode reply of
+                        Left e -> writeChan jobResp $ JobError e
+                        Right r -> writeChan jobResp r
+                    readyNotify
+                    worker
+                RenderFinished -> do
+                    send sock [] $ encode RenderFinished
+                    _ <- receive sock
+                    readyNotify
+                    worker
+                Shutdown -> do
+                    send sock [] $ encode Shutdown
+                    _ <- receive sock
+                    return ()
 
-    worker
+      worker
 
 networkRenderManager :: [String] -> Chan InfoEvent -> Chan JobRequest -> Chan JobResponse -> IO ()
 networkRenderManager nodes iChan jobReq jobResp = do
