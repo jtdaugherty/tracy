e8d31028e4713ff43533ebed193312359652e01b
Jonathan Daugherty <cygnus@foobox.com>
2015-01-11 19:16:48 -0800

    SceneBuilder: plumb frame number into primitive builder operations

--- src/Tracy/SceneBuilder.hs
+++ src/Tracy/SceneBuilder.hs
@@ -35,55 +35,55 @@ import Tracy.Samplers
 
 sceneFromDesc :: SceneDesc -> Int -> Either String (Scene ThinLens)
 sceneFromDesc sd fn =
-    Scene <$> (worldFromDesc       $ sd^.sceneDescWorld)
+    Scene <$> (worldFromDesc fn    $ sd^.sceneDescWorld)
           <*> (accelSchemeFromDesc $ sd^.sceneDescAccelScheme)
-          <*> (cameraFromDesc      $ sd^.sceneDescCamera)
+          <*> (cameraFromDesc fn   $ sd^.sceneDescCamera)
           <*> (tracerFromDesc      $ sd^.sceneDescTracer)
 
-worldFromDesc :: WorldDesc -> Either String World
-worldFromDesc wd =
-    World <$> (pure              $ wd^.wdViewPlane)
-          <*> (pure              $ wd^.wdBgColor)
-          <*> (objectsFromDesc   $ wd^.wdObjects)
-          <*> (lightsFromDesc    $ wd^.wdLights)
-          <*> (lightFromDesc     $ wd^.wdAmbient)
-          <*> (pure              $ wd^.wdWorldShadows)
+worldFromDesc :: Int -> WorldDesc -> Either String World
+worldFromDesc fn wd =
+    World <$> (pure               $ wd^.wdViewPlane)
+          <*> (pure               $ wd^.wdBgColor)
+          <*> (objectsFromDesc fn $ wd^.wdObjects)
+          <*> (lightsFromDesc fn  $ wd^.wdLights)
+          <*> (lightFromDesc fn   $ wd^.wdAmbient)
+          <*> (pure               $ wd^.wdWorldShadows)
 
-objectsFromDesc :: [ObjectDesc] -> Either String [Object]
-objectsFromDesc os = concat <$> sequenceA (objectFromDesc <$> os)
+objectsFromDesc :: Int -> [ObjectDesc] -> Either String [Object]
+objectsFromDesc fn os = concat <$> sequenceA (objectFromDesc fn <$> os)
 
 single :: Either a b -> Either a [b]
 single v = (:[]) <$> v
 
-objectFromDesc :: ObjectDesc -> Either String [Object]
-objectFromDesc (Sphere c r m) = single $ sphere c r <$> materialFromDesc m
-objectFromDesc (ConcaveSphere c r m) = single $ concaveSphere c r <$> materialFromDesc m
-objectFromDesc (Rectangle p0 a b m) = single $ rectangle p0 a b <$> materialFromDesc m
-objectFromDesc (Triangle v1 v2 v3 m) = single $ tri v1 v2 v3 <$> materialFromDesc m
-objectFromDesc (Box v1 v2 m) = single $ box v1 v2 <$> materialFromDesc m
-objectFromDesc (Plane c norm m) = single $ plane c norm <$> materialFromDesc m
-objectFromDesc (Mesh mDesc m) = single $ mesh mDesc <$> materialFromDesc m
-objectFromDesc (Grid os) = single $ grid <$> (concat <$> sequenceA (objectFromDesc <$> os))
-objectFromDesc (Instances oDesc pairs) =
+objectFromDesc :: Int -> ObjectDesc -> Either String [Object]
+objectFromDesc fn (Sphere c r m) = single $ sphere c r <$> materialFromDesc fn m
+objectFromDesc fn (ConcaveSphere c r m) = single $ concaveSphere c r <$> materialFromDesc fn m
+objectFromDesc fn (Rectangle p0 a b m) = single $ rectangle p0 a b <$> materialFromDesc fn m
+objectFromDesc fn (Triangle v1 v2 v3 m) = single $ tri v1 v2 v3 <$> materialFromDesc fn m
+objectFromDesc fn (Box v1 v2 m) = single $ box v1 v2 <$> materialFromDesc fn m
+objectFromDesc fn (Plane c norm m) = single $ plane c norm <$> materialFromDesc fn m
+objectFromDesc fn (Mesh mDesc m) = single $ mesh mDesc <$> materialFromDesc fn m
+objectFromDesc fn (Grid os) = single $ grid <$> (concat <$> sequenceA (objectFromDesc fn <$> os))
+objectFromDesc fn (Instances oDesc pairs) =
     let mkInstance o (mMatrix, mMat) =
             case mMat of
                 Nothing -> Right $ inst mMatrix Nothing o
-                Just mDesc -> inst mMatrix <$> (Just <$> materialFromDesc mDesc) <*> (pure o)
-    in case objectFromDesc oDesc of
+                Just mDesc -> inst mMatrix <$> (Just <$> materialFromDesc fn mDesc) <*> (pure o)
+    in case objectFromDesc fn oDesc of
           Left e -> Left e
           Right [o] -> sequenceA (mkInstance o <$> pairs)
           Right _ -> error "Error: Instances of (multiple) Instances not allowed"
 
-lightsFromDesc :: [LightDesc] -> Either String [Light]
-lightsFromDesc ls = sequenceA (lightFromDesc <$> ls)
+lightsFromDesc :: Int -> [LightDesc] -> Either String [Light]
+lightsFromDesc fn ls = sequenceA (lightFromDesc fn <$> ls)
 
-lightFromDesc :: LightDesc -> Either String Light
-lightFromDesc (Ambient s c) = Right $ ambientLight s c
-lightFromDesc (AmbientOccluder c min_amt s) = Right $ ambientOccluder c min_amt s
-lightFromDesc (Point sh ls c loc) = Right $ pointLight sh ls c loc
-lightFromDesc (Environment sh m) = environmentLight sh <$> (materialFromDesc m)
-lightFromDesc (Area sh oDesc p) =
-    case objectFromDesc oDesc of
+lightFromDesc :: Int -> LightDesc -> Either String Light
+lightFromDesc _ (Ambient s c) = Right $ ambientLight s c
+lightFromDesc _ (AmbientOccluder c min_amt s) = Right $ ambientOccluder c min_amt s
+lightFromDesc _ (Point sh ls c loc) = Right $ pointLight sh ls c loc
+lightFromDesc fn (Environment sh m) = environmentLight sh <$> (materialFromDesc fn m)
+lightFromDesc fn (Area sh oDesc p) =
+    case objectFromDesc fn oDesc of
       Left e -> Left e
       Right [o] -> Right $ areaLight sh o p
       Right _ -> Left "Could not create area light from multiple objects"
@@ -92,20 +92,20 @@ accelSchemeFromDesc :: AccelSchemeDesc -> Either String AccelScheme
 accelSchemeFromDesc NoScheme = Right noScheme
 accelSchemeFromDesc GridScheme = Right gridScheme
 
-materialFromDesc :: MaterialDesc -> Either String Material
-materialFromDesc (Matte c) = Right $ matteFromColor c
-materialFromDesc (Phong c ks e) = Right $ phongFromColor c ks e
-materialFromDesc (Reflective c ks e cr kr) = Right $ reflective c ks e cr kr
-materialFromDesc (GlossyReflective c ks e cr kr er) = Right $ glossyReflective c ks e cr kr er
-materialFromDesc (Emissive c e) = Right $ emissive c e
+materialFromDesc :: Int -> MaterialDesc -> Either String Material
+materialFromDesc _ (Matte c) = Right $ matteFromColor c
+materialFromDesc _ (Phong c ks e) = Right $ phongFromColor c ks e
+materialFromDesc _ (Reflective c ks e cr kr) = Right $ reflective c ks e cr kr
+materialFromDesc _ (GlossyReflective c ks e cr kr er) = Right $ glossyReflective c ks e cr kr er
+materialFromDesc _ (Emissive c e) = Right $ emissive c e
 
 tracerFromDesc :: TracerDesc -> Either String Tracer
 tracerFromDesc RayCastTracer = Right rayCastTracer
 tracerFromDesc AreaLightTracer = Right areaLightTracer
 tracerFromDesc WhittedTracer = Right whittedTracer
 
-cameraFromDesc :: CameraDesc -> Either String (Camera ThinLens)
-cameraFromDesc cd@(ThinLensCamera { }) =
+cameraFromDesc :: Int -> CameraDesc -> Either String (Camera ThinLens)
+cameraFromDesc _ cd@(ThinLensCamera { }) =
     Right $ thinLensCamera (cd^.thinLensEye)
                            (cd^.thinLensLookAt)
                            (cd^.thinLensUp)
