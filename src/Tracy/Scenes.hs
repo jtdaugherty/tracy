9f3246e958bd8b56196c85f5425245ffab49dcea
Jonathan Daugherty <cygnus@foobox.com>
2015-01-26 17:59:10 -0800

    Port table scene to YAML

--- src/Tracy/Scenes.hs
+++ src/Tracy/Scenes.hs
@@ -49,38 +49,6 @@ worldOcc os ls ambStr = WorldDesc { _wdViewPlane = defaultVp
                                   , _wdWorldShadows = True
                                   }
 
-tableScene :: SceneDesc
-tableScene =
-    let tableObj =  Mesh (MeshFile "meshes/table.ply") $ Phong (Colour (100.0/255.0) (73.0/255.0) 0) 0.5 100
-        monkeyObj = Mesh (MeshFile "meshes/monkey2.ply") $ GlossyReflective cCyan 0.7 10 cWhite 0.7 100
-        torusObj =  Mesh (MeshFile "meshes/torus.ply") $ Reflective (Colour 1 0.7 0.7) 0.9 1000 (Colour 1 0.7 0.7) 0.7
-        icoObj =   Mesh (MeshFile "meshes/ico1.ply") $ Matte cYellow
-        sphereObj = Sphere (V3 0.10225 4.449909 (-1.18322)) 0.811 $ Reflective (grey 0.3) 0.9 1000 cWhite 0.9
-        chairLeft = Mesh (MeshFile "meshes/chairLeft.ply") $ Phong (Colour 1 (79/255) 0) 1.0 100000
-        chairRight = Mesh (MeshFile "meshes/chairRight.ply") $ Phong (Colour 1 (79/255) 0) 1.0 100000
-
-        a = Rectangle (V3 (-2.5) 8 0) (V3 5 0 0) (V3 0 0 5) (Emissive cWhite 6)
-
-        p_bottom = Mesh (MeshFile "meshes/plane_bottom.ply") $ Matte cWhite
-        p_top = Mesh (MeshFile "meshes/plane_top.ply") $ Matte cWhite
-        p_left = Mesh (MeshFile "meshes/plane_left.ply") $ Matte (Colour (67/255) (151/255) (224/255))
-        p_right = Mesh (MeshFile "meshes/plane_right.ply") $ Matte (Colour (67/255) (151/255) (224/255))
-        p_back = Mesh (MeshFile "meshes/plane_back.ply") $ Matte cWhite
-        p_front = Mesh (MeshFile "meshes/plane_front.ply") $ Matte cWhite
-
-    in SceneDesc (world [ chairLeft, chairRight, tableObj
-                        , monkeyObj, torusObj, icoObj
-                        , sphereObj, a
-                        , p_bottom, p_back, p_left, p_right, p_top, p_front
-                        ] []) NoScheme
-                 (defCamera & thinLensRadius .~ (DoubleVal 0.0)
-                            & thinLensLookAt .~ (V3 0 3 0)
-                            & thinLensVpDist .~ 500
-                            & thinLensFpDist .~ 500
-                            & thinLensEye .~ (V3Val $ V3 0 5 3.5)
-                            )
-                 PathTracer
-
 rectangles :: SceneDesc
 rectangles =
     let r1 = Rectangle (V3 10 0 0) (V3 100 0 0) (V3 0 130 0)     (Phong cBlue  0.5 50)
@@ -108,7 +76,6 @@ areaLightScene =
 
 allScenes :: [(String, SceneDesc)]
 allScenes =
-    [ ("table",           tableScene)
-    , ("rectangles",      rectangles)
+    [ ("rectangles",      rectangles)
     , ("area-light",      areaLightScene)
     ]
