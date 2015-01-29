f6e485ae3f0489dcfd2bf4f2eae20eafcf0771f7
Jonathan Daugherty <jtd@galois.com>
2015-01-29 10:48:14 -0800

    Mix: optimize cases where mixture amount is 100% or 0%

--- src/Tracy/Materials/Mix.hs
+++ src/Tracy/Materials/Mix.hs
@@ -18,26 +18,44 @@ mix amt m1 m2 =
              , _getLe = mixGetLe amt m1 m2
              }
 
+theMix :: Double -> TraceM Color -> TraceM Color -> TraceM Color
+theMix amt a1 a2 = do
+    v1 <- a1
+    v2 <- a2
+    return $ (v1 * grey amt) + (v2 * (grey (1-amt)))
+
 mixShading :: Double -> Material -> Material -> Shade -> Tracer -> TraceM Color
-mixShading amt m1 m2 sh t = do
-    v1 <- (m1^.doShading) sh t
-    v2 <- (m2^.doShading) sh t
-    return $ (v1 * grey amt) + (v2 * (grey $ 1-amt))
+mixShading amt m1 m2 sh t
+  | amt == 1.0 = v1
+  | amt == 0.0 = v2
+  | otherwise = theMix amt v1 v2
+  where
+    v1 = (m1^.doShading) sh t
+    v2 = (m2^.doShading) sh t
 
 mixAreaShading :: Double -> Material -> Material -> Shade -> Tracer -> TraceM Color
-mixAreaShading amt m1 m2 sh t = do
-    v1 <- (m1^.doAreaShading) sh t
-    v2 <- (m2^.doAreaShading) sh t
-    return $ (v1 * grey amt) + (v2 * (grey $ 1-amt))
+mixAreaShading amt m1 m2 sh t
+  | amt == 1.0 = v1
+  | amt == 0.0 = v2
+  | otherwise = theMix amt v1 v2
+  where
+    v1 = (m1^.doAreaShading) sh t
+    v2 = (m2^.doAreaShading) sh t
 
 mixPathShading :: Double -> Material -> Material -> Shade -> Tracer -> TraceM Color
-mixPathShading amt m1 m2 sh t = do
-    v1 <- (m1^.doPathShading) sh t
-    v2 <- (m2^.doPathShading) sh t
-    return $ (v1 * grey amt) + (v2 * (grey $ 1-amt))
+mixPathShading amt m1 m2 sh t
+  | amt == 1.0 = v1
+  | amt == 0.0 = v2
+  | otherwise = theMix amt v1 v2
+  where
+    v1 = (m1^.doPathShading) sh t
+    v2 = (m2^.doPathShading) sh t
 
 mixGetLe :: Double -> Material -> Material -> Shade -> Color
-mixGetLe amt m1 m2 sh = (v1 * grey amt) + (v2 * (grey $ 1-amt))
+mixGetLe amt m1 m2 sh
+  | amt == 1.0 = v1
+  | amt == 0.0 = v2
+  | otherwise = (v1 * grey amt) + (v2 * (grey $ 1-amt))
     where
       v1 = (m1^.getLe) sh
       v2 = (m2^.getLe) sh
