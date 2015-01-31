36685dae1b9f981b4840fd3d1ea53a0c05011dd3
Jonathan Daugherty <cygnus@foobox.com>
2015-01-31 12:51:51 -0800

    bench: benchmark more samplers

--- programs/BenchMain.hs
+++ programs/BenchMain.hs
@@ -1,17 +1,26 @@
 module Main where
 
 import Criterion.Main
+import Control.DeepSeq (NFData)
 import System.Random.MWC
 
+import Tracy.Types
 import Tracy.Samplers
+
+mkSamplerGroup :: (NFData a) => GenIO -> String -> Sampler a -> Benchmark
+mkSamplerGroup gen name s =
+    bgroup name [ bench "4"  $ nfIO $ runSampler s gen 4
+                , bench "10" $ nfIO $ runSampler s gen 10
+                , bench "20" $ nfIO $ runSampler s gen 20
+                , bench "32" $ nfIO $ runSampler s gen 32
+                ]
 
 main :: IO ()
 main = do
     gen <- createSystemRandom
-    defaultMain [ bgroup "regular" [ bench "1"  $ whnfIO $ runSampler regular gen 1
-                                   , bench "2"  $ whnfIO $ runSampler regular gen 2
-                                   , bench "3"  $ whnfIO $ runSampler regular gen 3
-                                   , bench "4"  $ whnfIO $ runSampler regular gen 4
-                                   , bench "5"  $ whnfIO $ runSampler regular gen 5
-                                   ]
-                ] 
+    defaultMain [ mkSamplerGroup gen "regular" regular
+                , mkSamplerGroup gen "pureRandom" pureRandom
+                , mkSamplerGroup gen "jittered" jittered
+                , mkSamplerGroup gen "multiJittered" multiJittered
+                , mkSamplerGroup gen "correlatedMultiJittered" correlatedMultiJittered
+                ]
