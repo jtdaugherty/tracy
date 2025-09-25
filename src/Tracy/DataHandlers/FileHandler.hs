3c1f12297b7487bb64eeb82286e918539ecf08ae
Jonathan Daugherty <cygnus@foobox.com>
2025-09-25 11:06:54 -0700

    FileHandler: resolve warning and comment out additional ffmpeg-related code

--- src/Tracy/DataHandlers/FileHandler.hs
+++ src/Tracy/DataHandlers/FileHandler.hs
@@ -8,17 +8,18 @@ module Tracy.DataHandlers.FileHandler
   where
 
 import Control.Concurrent.Chan
+import Data.Maybe (fromJust)
 import qualified Data.Map as M
 import qualified Data.ByteString as B
 import Codec.BMP
 import qualified Data.Vector.Storable as SV
-import Data.Word (Word8)
-import Data.Colour
+-- import Data.Word (Word8)
+-- import Data.Colour
 import System.IO (stderr)
 import System.IO.Silently
 
 -- import Codec.FFmpeg (initFFmpeg, defaultParams, imageWriter)
-import qualified Codec.Picture as JP
+-- import qualified Codec.Picture as JP
 
 import Tracy.Types
 import Tracy.Util
@@ -49,7 +50,7 @@ fileHandler chan = do
                   oldCnt = m M.! startRow
                   newCnt = oldCnt + sc
               mergeChunks oldCnt newCnt startRow merged rs
-              work $ M.alter (\(Just v) -> Just (v + sc)) startRow m
+              work $ M.alter (\val -> Just (fromJust val + sc)) startRow m
             DFinished frameNum -> do
                 vec <- vectorFromMergeBuffer merged
                 let vec2 = SV.map maxToOne vec
@@ -70,7 +71,7 @@ writeImage dat cols rows filename = do
   writeBMP filename img
 
 setupFrameOutput :: (Frame, Frame) -> (Int, Int) -> String -> IO (SV.Vector Color -> Frame -> IO (), IO ())
-setupFrameOutput (firstFrame, lastFrame) (cols, rows) sceneName = hSilence [stderr] $ do
+setupFrameOutput _ (cols, rows) sceneName = hSilence [stderr] $ do
   -- Set up frame writer: if we are rendering more than one frame,
   -- assume we are writing a movie and set up a streaming video encoder.
   -- case lastFrame > firstFrame of
@@ -87,18 +88,18 @@ setupFrameOutput (firstFrame, lastFrame) (cols, rows) sceneName = hSilence [stde
           let writeFrame vec fn = writeImage vec cols rows (buildImageFilename sceneName fn)
           return (writeFrame, return ())
 
-juicyImageFromVec :: Int -> Int -> SV.Vector Color -> JP.Image JP.PixelRGB8
-juicyImageFromVec w h colorVec = flipImageVertically $ JP.Image w h componentVec
-    where
-        componentVec = SV.concatMap toWordVec colorVec
+-- juicyImageFromVec :: Int -> Int -> SV.Vector Color -> JP.Image JP.PixelRGB8
+-- juicyImageFromVec w h colorVec = flipImageVertically $ JP.Image w h componentVec
+--     where
+--         componentVec = SV.concatMap toWordVec colorVec
 
-flipImageVertically :: (JP.Pixel a) => JP.Image a -> JP.Image a
-flipImageVertically image = JP.generateImage flippedPixel (JP.imageWidth image) (JP.imageHeight image)
-    where
-        flippedPixel x y = JP.pixelAt image x ((JP.imageHeight image) - 1 - y)
+-- flipImageVertically :: (JP.Pixel a) => JP.Image a -> JP.Image a
+-- flipImageVertically image = JP.generateImage flippedPixel (JP.imageWidth image) (JP.imageHeight image)
+--     where
+--         flippedPixel x y = JP.pixelAt image x ((JP.imageHeight image) - 1 - y)
 
-toWordVec :: Colour -> SV.Vector Word8
-toWordVec (Colour r g b) = SV.fromList [ (toEnum $ fromEnum (r * 255.0))
-                                       , (toEnum $ fromEnum (g * 255.0))
-                                       , (toEnum $ fromEnum (b * 255.0))
-                                       ]
+-- toWordVec :: Colour -> SV.Vector Word8
+-- toWordVec (Colour r g b) = SV.fromList [ (toEnum $ fromEnum (r * 255.0))
+--                                        , (toEnum $ fromEnum (g * 255.0))
+--                                        , (toEnum $ fromEnum (b * 255.0))
+--                                        ]
